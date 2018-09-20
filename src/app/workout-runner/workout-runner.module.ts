18a1ba258fd679a4d76a9239a4bb69cbfe6cdcc9
Camille Blondel <blondel.camille76@gmail.com>
2018-09-20 11:28:07 +0200

    add and implement second-to-time pipe

--- src/app/workout-runner/workout-runner.module.ts
+++ src/app/workout-runner/workout-runner.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { WorkoutRunnerComponent } from './workout-runner.component';
 import { ExerciseDescriptionComponent } from './exercise-description/exercise-description.component';
 import { VideoPlayerComponent } from './video-player/video-player.component';
+import { SecondToTimePipe } from './shared/second-to-time.pipe';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   exports: [WorkoutRunnerComponent],
-  declarations: [WorkoutRunnerComponent, ExerciseDescriptionComponent, VideoPlayerComponent]
+  declarations: [WorkoutRunnerComponent, ExerciseDescriptionComponent, VideoPlayerComponent, SecondToTimePipe]
 })
 export class WorkoutRunnerModule { }
